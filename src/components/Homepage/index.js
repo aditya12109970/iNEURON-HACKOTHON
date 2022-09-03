2946acae9e3be295adbfbba3cc9ba9885e12a1cb
Rohit Kumar <rk212300@outlook.com>
2022-09-03 18:00:16 +0530

    initial commit

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+const Homepage = () => {
+    return (
+        <div>
+            <span class="v7_58">T∞GETHER</span><span class="v7_61">Meet anytime, anywhere</span>
+        </div >
+    )
+}
+
+export default Homepage
